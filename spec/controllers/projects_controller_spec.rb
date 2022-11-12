9874f3c97ad2b37f6d4bf78ac54fdc4aadc26891
HFelixH <felix-hoang@outlook.com>
2022-11-13 08:59:44 +1030

    Attempt to create render test

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -3,8 +3,7 @@ require_relative '../support/devise'
 
 RSpec.describe ProjectsController, type: :controller do
   describe 'GET /' do
-    login_user
-
+    login_use
     context 'login user and re-direct to index' do
       it 'should return 200:OK' do
         get :index
@@ -13,3 +12,22 @@ RSpec.describe ProjectsController, type: :controller do
     end
   end
 end
+
+# RSpec.describe ProjectsController, type: :controller do
+#   before :each do
+#     @user = FactoryBot.create(:user)
+#   end
+
+#   let(:attributes) do 
+#     { user: @user, 
+#       name: 'Testing', 
+#       description: 'Testing is important and it very essential' 
+#     }
+#   end
+
+#   it 'renders the show template' do
+#     project = Project.create!(attributes)
+#     get :show, id: project.id
+#     expect(response).to render_template(:show)
+#   end
+# end
