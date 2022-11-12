61d315ee5388306991f6cbe840f99b130eb2c968
HFelixH <felix-hoang@outlook.com>
2022-11-13 08:34:39 +1030

    Test Task for Validation faliure

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+require_relative '../support/devise'
+
+RSpec.describe Task, type: :model do
+  let(:task) { Task.create(name: 'Testing Task', description: 'Testing Testing 123!', status: 'Complete') }
+
+  it 'should be invalid without an name' do
+    task.name = nil
+    expect(task.valid?).to be false
+  end
+
+  it 'should be invalid without an description' do
+    task.description = nil
+    expect(task.valid?).to be false
+  end
+
+  it 'should be invalid without a status' do
+    task.status = nil
+    expect(task.valid?).to be false
+  end
+end
